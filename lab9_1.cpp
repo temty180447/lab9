44611c0b7dcbbb26bbc49537914b60db465c8257
temty=180447 <tgtv2547@gmail.com>
2023-12-15 17:32:19 +0700

    lab

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -2,10 +2,15 @@
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
-int main(){	
-	cout << "Enter initial loan: ";
+int main(){
+    double PrevBalance, per,total ,Payment, NewBalance, Interest;
+    int i = 1;
+	cout << "Enter initial loan: "; 
+	cin >> PrevBalance;
 	cout << "Enter interest rate per year (%): ";
+	cin >> per;
 	cout << "Enter amount you can pay per year: ";
+	cin >> Payment;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -20,14 +25,24 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
-	
+	while(PrevBalance>0){
+    NewBalance=(PrevBalance+(PrevBalance*(per/100)))-Payment;
+    Interest = PrevBalance*(per/100);
+    total = PrevBalance+Interest;
+	cout << fixed << setprecision(2);
+	if(NewBalance<0){
+	    Payment=total;
+	    NewBalance = 0;
+	}
+	cout << setw(13) << left << i; 
+	cout << setw(13) << left << PrevBalance;
+	cout << setw(13) << left << Interest;
+	cout << setw(13) << left << total;
+	cout << setw(13) << left << Payment;
+	cout << setw(13) << left << NewBalance;
+	cout << "\n";
+	i++;
+	PrevBalance=NewBalance;
+	}
 	return 0;
 }
--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,8 +1,18 @@
 #include<iostream>
 using namespace std;
 
-//Write the function printO() here
-
+void printO(int N, int M){
+        if(N<=0||M<=0){
+                cout << "Invalid input";
+            }
+        for(int i = 0 ; i < N ; i++){
+            for(int j = 0 ; j < M ; j++){
+                cout << "O";
+            }
+            cout << endl;
+        }
+    }
+	
 int main(){
 	
 	printO(2,2);
